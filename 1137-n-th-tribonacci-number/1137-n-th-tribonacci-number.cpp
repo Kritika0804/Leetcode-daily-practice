fc1fe46f5fb547e4b54e304ccf4f19bf07c8b99d
Kritika Choudhary <88735718+Kritika0804@users.noreply.github.com>
2022-09-10 13:03:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (53.40%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -21,9 +21,18 @@ public:
     
     
     int tribonacci(int n) {
-        for(int i=0;i<=n;i++){
-            dp[i]=-1;
+        // for(int i=0;i<=n;i++){
+        //     dp[i]=-1;
+        // }
+        // return helper(n);
+        
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        // dp[3]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
-        return helper(n);
+        return dp[n];
     }
 };
