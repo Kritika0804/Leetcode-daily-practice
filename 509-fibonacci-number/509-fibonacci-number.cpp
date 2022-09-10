ac7d52013f848580ee64e369f8f1db0101d67bc9
Kritika Choudhary <88735718+Kritika0804@users.noreply.github.com>
2022-09-10 12:21:35 +0530

    Time: 5 ms (49.16%), Space: 5.9 MB (40.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -16,13 +16,23 @@ public:
     
     
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
+        int dp[40];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
         
-        
-        for(int i=0;i<=n;i++){
-            dp[i]=-1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return helper(n);
+        return dp[n];
     }
 };
+
+// if(n==0) return 0;
+//         if(n==1) return 1;
+        
+        
+//         for(int i=0;i<=n;i++){
+//             dp[i]=-1;
+//         }
+//         return helper(n);
