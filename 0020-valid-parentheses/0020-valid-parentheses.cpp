7547b30c4f822d027954526a72247b90a086d215
Kritika Choudhary <88735718+Kritika0804@users.noreply.github.com>
2023-05-14 11:52:26 +0530

    Time: 4 ms (39.15%), Space: 6.1 MB (97.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c:s){
+            if(c=='(' || c=='[' || c=='{'){
+                st.push(c);
+            }
+            else if(!st.empty()){
+                if(c==')' && st.top()=='('){
+                    st.pop();
+            }
+            else if(c==']' && st.top()=='['){
+                    st.pop();
+            }
+            else if(c=='}' && st.top()=='{'){
+                    st.pop();
+            }
+            else{
+                return false;
+            }
+
+            }
+            else{
+                return false;
+            }
+            
+        }
+        if(st.empty()){
+                return true;
+        }
+        return false;
+        
+    }
+};
