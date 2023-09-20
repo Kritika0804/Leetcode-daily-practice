0f79d80ee51c73f7abef68735061cc350236442f
Kritika Choudhary <88735718+Kritika0804@users.noreply.github.com>
2023-09-20 10:09:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (7.17%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string defangIPaddr(string address) {
         string ans="";
-        // int i=0;
-       for(int i=0;i<address.length();i++){
+        int i=0;
+       while(i!=address.length()){
             if(address[i]=='.'){
                 ans=ans+"[.]";
             }
             else{
                 ans=ans+address[i];
             }
-            
+            i++;
         }
         return ans;
     }
