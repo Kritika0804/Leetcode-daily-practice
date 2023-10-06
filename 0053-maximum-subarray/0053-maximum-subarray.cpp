74b9495bf0e639e125421d685ecc7afed99d14d7
Kritika Choudhary <88735718+Kritika0804@users.noreply.github.com>
2023-10-06 11:40:23 +0530

    Time: 84 ms (51.04%), Space: 68 MB (21.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
-        if(arr.size()==1){
-            return arr[0];
-        }
+        // if(arr.size()==1){
+        //     return arr[0];
+        // }
         int currsum=arr[0];
         int maxsum=arr[0];
         for(int i=1;i<arr.size();i++){
